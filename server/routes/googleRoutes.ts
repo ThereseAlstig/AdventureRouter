dda598663ebe98fb99b54848ba4cc0eb866154e0
Therese Ålstig <128577528+ThereseAlstig@users.noreply.github.com>
2024-12-04 11:17:29 +0100

    callbackURLÖ

--- server/routes/googleRoutes.ts
+++ server/routes/googleRoutes.ts
@@ -4,12 +4,14 @@ import { googleAuthCallback } from '../controllers/googleController';
 
 const router = express.Router();
 
+const callback = process.env.GOOGLE_REDIRECT_URL || 'https://adventure-router.vercel.app/';
+
 router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
 router.get('/google/callback', passport.authenticate('google', { session: false }), (req, res) => {
     console.log('User authenticated:', req.user);
 
     // Omdirigera användaren till frontend
-    res.redirect('http://localhost:5173/my-page'); // Ändra till din frontend-URL
+    res.redirect(callback); // Ändra till din frontend-URL
 });
 
 export default router;

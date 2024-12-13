4cd69bca2e642c47d430da6d16aad851c5320ada
Therese Ålstig <128577528+ThereseAlstig@users.noreply.github.com>
2024-12-13 18:03:20 +0100

    ny

--- client/src/pages/sharedAdventure.tsx
+++ client/src/pages/sharedAdventure.tsx
@@ -1,63 +1,16 @@
 
-import { useEffect, useState } from "react";
-import { GetSharedAdventures } from "../api/getSharedTrips";
+
 import { SharedTrips } from "../components/sharedTrips";
 import { LoadScript } from "@react-google-maps/api";
-import { fetchTripImage } from "../api/fetchImg";
+
 
 export const SharedAdventure = () => {
-    const [trips, setTrips] = useState<any[]>([]);
-    const [error, setError] = useState<string | null>(null);
-    const [tripImages, setTripImages] = useState<{ [key: number]: string | null }>({});
+   
+
+   
 
     const apiKey = import.meta.env.VITE_REACT_GOOGLE_MAPS_API_KEY || "YOUR_API_KEY";
-    useEffect(() => {
-      const loadTrips = async () => {
-        try {
-          const cachedTrips = localStorage.getItem('trips');
-          const cachedImages = localStorage.getItem('tripImages');
-    
-          if (cachedTrips && cachedImages) {
-            setTrips(JSON.parse(cachedTrips));
-            setTripImages(JSON.parse(cachedImages));
-            return;
-          }
-    
-          const tripsData = await GetSharedAdventures();
-    
-          if (!tripsData) {
-            throw new Error('Failed to fetch trips');
-          }
-    
-          setTrips(tripsData);
-          localStorage.setItem('trips', JSON.stringify(tripsData));
-    
-          const images = await Promise.all(
-            tripsData.map(async (trip: any) => {
-              const imageUrl = await fetchTripImage(trip.trip_id);
-              return { tripId: trip.trip_id, imageUrl };
-            })
-          );
-    
-          const imageMap = images.reduce((acc, curr) => {
-            acc[curr.tripId] = curr.imageUrl;
-            return acc;
-          }, {} as { [key: number]: string | null });
-    
-          setTripImages(imageMap);
-          localStorage.setItem('tripImages', JSON.stringify(imageMap));
-        } catch (err: any) {
-          setError(err.message);
-        }
-      };
-    
-      loadTrips();
-    }, []);
-    
-  
-    if (error) {
-      return <p>Error loading trips: {error}</p>;
-    }
+   
 
 
 
